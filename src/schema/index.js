f803ea42e2f94d8e65dc8f0c0bb0bee3f6c95302
SCabralTenet <scabral@tenet.io>
2024-03-27 15:49:42 -0300

    Add mutation to import competition

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,8 +1,11 @@
 const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+
 const playerQueries = require('./queries/player.queries');
 const coachQueries = require('./queries/coach.queries');
 const teamQueries = require('./queries/team.queries');
 const competitionQueries = require('./queries/competition.queries');
+
+const competitionMutations = require('./mutations/competition.mutations');
 
 // Define the Root Query with all queries combined.
 const RootQuery = new GraphQLObjectType({
@@ -16,10 +19,16 @@ const RootQuery = new GraphQLObjectType({
 });
 
 // Define the Root Mutation with all mutations.
-//const RootMutation = new GraphQLObjectType();
+const RootMutation = new GraphQLObjectType({
+    name: 'RootMutation',
+    fields: {
+        ...competitionMutations
+    }
+});
 
 const schema = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: RootMutation
 });
 
 module.exports = schema;
--- src/schema/mutations/competition.mutations.js
+++ src/schema/mutations/competition.mutations.js
@@ -1,20 +1,86 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean } = graphql;
+const { GraphQLID } = graphql;
 const CompetitionType = require('./../types/competition.type');
+const competitionsService = require('./../../services/competitions.service');
+const { Competition, Team, Player, Coach } = require('./../../database/models/index');
 
-const CompetitionMutation = new GraphQLObjectType({
-  name: "CompetitionMutationType",
-  type: "Mutation",
-  fields: {
-    addCompetition: {
-      type: CompetitionType,
-      args: {
-        id: { type: GraphQLID }
-      },
-      resolve(parentValue, args) {
+
+module.exports = {
+  addCompetition: {
+    type: CompetitionType,
+    args: {
+      id: { type: GraphQLID }
+    },
+    async resolve(parentValue, args) {
+      const { id } = args;
+      const competitionResult = await competitionsService.getCompetitionByCode(id);
+
+      const [competition, isCreated] = await Competition.findOrCreate(
+        {
+          where: {
+            id
+          },
+          defaults: {
+            id: competitionResult.id,
+            name: competitionResult.name,
+            code: competitionResult.code,
+            areaName: competitionResult.area.name
+          }
+        });
+
+      const { teams } = await competitionsService.getTeamsForCompetition(id);
+
+      for (let teamResult of teams) {
+        await Team.findOrCreate({
+          where: { id: teamResult.id },
+          defaults: {
+            id: teamResult.id,
+            name: teamResult.name,
+            shortName: teamResult.shortName,
+            tla: teamResult.tla,
+            areaName: teamResult.area.name,
+            address: teamResult.address
+          }
+        });
+
+        await competition.addTeam(teamResult.id);
+
+        const { coach, squad } = teamResult;
+        if (coach) {
+          await Coach.findOrCreate({
+            where: { id: coach.id },
+            defaults: {
+              id: coach.id,
+              name: `${coach.firstName} ${coach.lastName}`,
+              dateOfBirth: coach.dateOfBirth,
+              nationality: coach.nationality,
+              teamId: teamResult.id
+            }
+          }
+          );
+        }
+
+        for (let player of squad) {
+          await Player.findOrCreate({
+            where: { id: player.id },
+            defaults: {
+              id: player.id,
+              name: player.name,
+              position: player.position,
+              dateOfBirth: player.dateOfBirth,
+              nationality: player.nationality,
+              teamId: teamResult.id
+            }
+          });
+        }
+      }
+
+      return {
+        id: competition.id,
+        name: competition.name,
+        code: competition.code,
+        areaName: competition.areaName
       }
     }
   }
-});
-
-exports.mutation = CompetitionMutationType;
+};
